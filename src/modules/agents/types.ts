9f18f485a58e4b87fee77bdb87738a2c078a2864
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-07-01 17:48:54 +0300

    11: Agent Form

---
+++ src/modules/agents/types.ts
@@ -0,0 +1,6 @@
+import { inferRouterOutputs } from "@trpc/server";
+// AppRouter we used to access the agentsRouter to get the type of the output of one agent
+import type { AppRouter } from "@/trpc/routers/_app";
+
+
+export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
