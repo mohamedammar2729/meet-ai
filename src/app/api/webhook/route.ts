87562de0258536735f73ee628a87ead461740412
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-07-07 01:18:45 +0300

    24: Background Jobs

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,7 +1,7 @@
 import {
-  //   CallEndedEvent,
-  //   CallTranscriptionReadyEvent,
-  //   CallRecordingReadyEvent,
+  CallEndedEvent,
+  CallTranscriptionReadyEvent,
+  CallRecordingReadyEvent,
   CallSessionParticipantLeftEvent,
   CallSessionStartedEvent,
 } from '@stream-io/node-sdk';
@@ -11,6 +11,7 @@ import { NextResponse, NextRequest } from 'next/server';
 import { db } from '@/db';
 import { agents, meetings } from '@/db/schema';
 import { streamVideo } from '@/lib/stream-video';
+import { inngest } from '@/inngest/client';
 
 // first we will develope method to verify the signature
 // because this will be not protected via our auth system, it will protected via a signature
@@ -105,7 +106,7 @@ export async function POST(request: NextRequest) {
     const call = streamVideo.video.call('default', meetingId);
     const realtimeClient = await streamVideo.video.connectOpenAi({
       call,
-      openAiApiKey: process.env.OPEN_AI_API_KEY!,
+      openAiApiKey: process.env.OPENAI_API_KEY!,
       agentUserId: existingAgent.id,
     });
 
@@ -128,6 +129,66 @@ export async function POST(request: NextRequest) {
     // this is needed because when agent left the call, we need to end it
     const call = streamVideo.video.call('default', meetingId);
     await call.end();
+  } else if (eventType === 'call.session_ended') {
+    const event = payload as CallEndedEvent;
+    const meetingId = event.call.custom?.meetingId;
+
+    if (!meetingId) {
+      return NextResponse.json(
+        { error: 'Meeting ID is missing in the event' },
+        { status: 400 }
+      );
+    }
+
+    await db
+      .update(meetings)
+      .set({ status: 'processing', endedAt: new Date() })
+      .where(and(eq(meetings.id, meetingId), eq(meetings.status, 'active')));
+  } else if (eventType === 'call.transcription_ready') {
+    const event = payload as CallTranscriptionReadyEvent;
+    const meetingId = event.call_cid?.split(':')[1];
+    if (!meetingId) {
+      return NextResponse.json(
+        { error: 'Meeting ID is missing in the event' },
+        { status: 400 }
+      );
+    }
+    // update the meeting status to processing
+    const [updatedMeeting] = await db
+      .update(meetings)
+      .set({ transcriptUrl: event.call_transcription.url })
+      .where(eq(meetings.id, meetingId))
+      .returning();
+
+    if (!updatedMeeting) {
+      return NextResponse.json(
+        { error: 'Meeting not found or already processed' },
+        { status: 404 }
+      );
+    }
+
+    await inngest.send({
+      name: 'meetings/processing',
+      data: {
+        meetingId: updatedMeeting.id,
+        transcriptUrl: updatedMeeting.transcriptUrl,
+      },
+    });
+  } else if (eventType === 'call.recording_ready') {
+    const event = payload as CallRecordingReadyEvent;
+    const meetingId = event.call_cid?.split(':')[1];
+
+    if (!meetingId) {
+      return NextResponse.json(
+        { error: 'Meeting ID is missing in the event' },
+        { status: 400 }
+      );
+    }
+    // update the meeting status to processing
+    await db
+      .update(meetings)
+      .set({ recordingUrl: event.call_recording.url })
+      .where(eq(meetings.id, meetingId));
   }
 
   return NextResponse.json({ status: 'ok' });
