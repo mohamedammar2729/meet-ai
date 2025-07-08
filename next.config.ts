37b90bd8f5062096aaffdd61a08f3595903d76f9
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-07-08 04:49:04 +0300

    31: production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return[{
+      source: '/',
+      destination: '/meetings',
+      permanent: false,
+    }]
+  }
 };
 
 export default nextConfig;
